782657e160e0af936aa1bb7aa103d353682c2713
Tom Lagier <tom@gliffy.com>
2024-06-26 16:53:40 -0700

    More memoKey manipulation

--- src/slider.js
+++ src/slider.js
@@ -1,22 +1,22 @@
-"use strict";
+'use strict';
 
-import React from "react";
-import { InnerSlider } from "./inner-slider";
-import json2mq from "json2mq";
-import defaultProps from "./default-props";
-import { canUseDOM } from "./utils/innerSliderUtils";
-const enquire = canUseDOM() && require("enquire.js");
+import React from 'react';
+import { InnerSlider } from './inner-slider';
+import json2mq from 'json2mq';
+import defaultProps from './default-props';
+import { canUseDOM } from './utils/innerSliderUtils';
+const enquire = canUseDOM() && require('enquire.js');
 
 export default class Slider extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      breakpoint: null
+      breakpoint: null,
     };
     this._responsiveMediaHandlers = [];
   }
 
-  innerSliderRefHandler = ref => (this.innerSlider = ref);
+  innerSliderRefHandler = (ref) => (this.innerSlider = ref);
 
   media(query, handler) {
     // javascript handler for  css media query
@@ -33,7 +33,7 @@ export default class Slider extends React.Component {
     //}
     if (this.props.responsive) {
       let breakpoints = this.props.responsive.map(
-        breakpt => breakpt.breakpoint
+        (breakpt) => breakpt.breakpoint
       );
       // sort them in increasing order of their numerical value
       breakpoints.sort((x, y) => x - y);
@@ -42,11 +42,14 @@ export default class Slider extends React.Component {
         // media query for each breakpoint
         let bQuery;
         if (index === 0) {
-          bQuery = json2mq({ minWidth: 0, maxWidth: breakpoint });
+          bQuery = json2mq({
+            minWidth: 0,
+            maxWidth: breakpoint,
+          });
         } else {
           bQuery = json2mq({
             minWidth: breakpoints[index - 1] + 1,
-            maxWidth: breakpoint
+            maxWidth: breakpoint,
           });
         }
         // when not using server side rendering
@@ -58,7 +61,9 @@ export default class Slider extends React.Component {
 
       // Register media query for full screen. Need to support resize from small to large
       // convert javascript object to media query string
-      let query = json2mq({ minWidth: breakpoints.slice(-1)[0] });
+      let query = json2mq({
+        minWidth: breakpoints.slice(-1)[0],
+      });
 
       canUseDOM() &&
         this.media(query, () => {
@@ -80,21 +85,25 @@ export default class Slider extends React.Component {
   slickGoTo = (slide, dontAnimate = false) =>
     this.innerSlider.slickGoTo(slide, dontAnimate);
 
-  slickPause = () => this.innerSlider.pause("paused");
+  slickPause = () => this.innerSlider.pause('paused');
 
-  slickPlay = () => this.innerSlider.autoPlay("play");
+  slickPlay = () => this.innerSlider.autoPlay('play');
 
   render() {
     var settings;
     var newProps;
     if (this.state.breakpoint) {
       newProps = this.props.responsive.filter(
-        resp => resp.breakpoint === this.state.breakpoint
+        (resp) => resp.breakpoint === this.state.breakpoint
       );
       settings =
-        newProps[0].settings === "unslick"
-          ? "unslick"
-          : { ...defaultProps, ...this.props, ...newProps[0].settings };
+        newProps[0].settings === 'unslick'
+          ? 'unslick'
+          : {
+              ...defaultProps,
+              ...this.props,
+              ...newProps[0].settings,
+            };
     } else {
       settings = { ...defaultProps, ...this.props };
     }
@@ -103,7 +112,7 @@ export default class Slider extends React.Component {
     if (settings.centerMode) {
       if (
         settings.slidesToScroll > 1 &&
-        process.env.NODE_ENV !== "production"
+        process.env.NODE_ENV !== 'production'
       ) {
         console.warn(
           `slidesToScroll should be equal to 1 in centerMode, you are using ${settings.slidesToScroll}`
@@ -113,14 +122,17 @@ export default class Slider extends React.Component {
     }
     // force showing one slide and scrolling by one if the fade mode is on
     if (settings.fade) {
-      if (settings.slidesToShow > 1 && process.env.NODE_ENV !== "production") {
+      if (
+        settings.slidesToShow > 1 &&
+        process.env.NODE_ENV !== 'production'
+      ) {
         console.warn(
           `slidesToShow should be equal to 1 when fade is true, you're using ${settings.slidesToShow}`
         );
       }
       if (
         settings.slidesToScroll > 1 &&
-        process.env.NODE_ENV !== "production"
+        process.env.NODE_ENV !== 'production'
       ) {
         console.warn(
           `slidesToScroll should be equal to 1 when fade is true, you're using ${settings.slidesToScroll}`
@@ -131,12 +143,14 @@ export default class Slider extends React.Component {
     }
 
     // makes sure that children is an array, even when there is only 1 child
-    let children = React.Children.toArray(this.props.children);
+    let children = React.Children.toArray(
+      this.props.children
+    );
 
     // Children may contain false or null, so we should filter them
     // children may also contain string filled with spaces (in certain cases where we use jsx strings)
-    children = children.filter(child => {
-      if (typeof child === "string") {
+    children = children.filter((child) => {
+      if (typeof child === 'string') {
         return !!child.trim();
       }
       return !!child;
@@ -166,19 +180,26 @@ export default class Slider extends React.Component {
         j += settings.slidesPerRow
       ) {
         let row = [];
-        for (let k = j; k < j + settings.slidesPerRow; k += 1) {
-          if (settings.variableWidth && children[k].props.style) {
+        for (
+          let k = j;
+          k < j + settings.slidesPerRow;
+          k += 1
+        ) {
+          if (
+            settings.variableWidth &&
+            children[k].props.style
+          ) {
             currentWidth = children[k].props.style.width;
           }
           if (k >= children.length) break;
           row.push(
             React.cloneElement(children[k], {
-              key: 100 * i + 10 * j + k,
+              key: children[k].props.memoKey,
               tabIndex: -1,
               style: {
                 width: `${100 / settings.slidesPerRow}%`,
-                display: "inline-block"
-              }
+                display: 'inline-block',
+              },
             })
           );
         }
@@ -191,14 +212,21 @@ export default class Slider extends React.Component {
           </div>
         );
       } else {
-        newChildren.push(<div key={i}>{newSlide}</div>);
+        newChildren.push(
+          <div key={children[i].props.memoKey}>
+            {newSlide}
+          </div>
+        );
       }
     }
 
-    if (settings === "unslick") {
-      const className = "regular slider " + (this.props.className || "");
+    if (settings === 'unslick') {
+      const className =
+        'regular slider ' + (this.props.className || '');
       return <div className={className}>{children}</div>;
-    } else if (newChildren.length <= settings.slidesToShow) {
+    } else if (
+      newChildren.length <= settings.slidesToShow
+    ) {
       settings.unslick = true;
     }
     return (
